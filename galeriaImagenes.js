32c3b1acf6b589200689bb083e35b02aac269d72
Ezevo022 <eze.22.xxi@gmail.com>
2025-05-01 17:16:30 -0300

    Se termina pagina galeriaImagenes

---
+++ galeriaImagenes.js
@@ -0,0 +1,26 @@
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modalImg');
+const cerrarBtn = document.getElementById('cerrar');
+
+// Al hacer click en cada imagen se captura su ubicacion y texto
+// alternativo para ampliarlo luego.
+// Como genera un array se captura al seleccionar cualquier elemento.
+document.querySelectorAll('.galeria img').forEach(img => {
+    img.addEventListener('click', () => {
+        modal.style.display = 'flex';
+        modalImg.src = img.src;
+        modalImg.alt = img.alt;
+    });
+});
+
+cerrarBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+// Se agrega evento para cerrar la ampliacion en caso de hacer click
+// fuera de la imagen por mas que no sea en el boton de X.
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
